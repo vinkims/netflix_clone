2d99a81a6989c002fad1a0eb0be54a35b31aa404
vinkims <vincekigen@gmail.com>
2023-06-09 14:00:12 +0300

    feat(API)- Add API connection and display movies

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "b4f664b5f9ff26b56896f5b2ae071d20";
+
+let img_url ="https://image.tmdb.org/t/p/w500";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
+let original_img_url = "https://image.tmdb.org/t/p/original";
+let movie_detail_http = "https://api.themoviedb.org/3/movie";
